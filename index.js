b4608d6c31cefcc678463895b2bf57d54df6a67e
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-05-11 19:55:06 +0900

    feat: pagenation api 구현

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const jsonServer = require("json-server");
 const cors = require("cors");
 
+const db = require("./db.json");
+
+console.log(db);
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
@@ -8,6 +12,25 @@ const middlewares = jsonServer.defaults();
 var corsOptions = {
   origin: "*",
 };
+
+server.get("/product-list/:pageNo", (req, res) => {
+  const { pageNo } = req.params;
+  if (!pageNo) res.status(200).send("BAD REQUEST: NEED PAGE NUMBER PARAMETER");
+
+  const { products } = db;
+  const productsPerPage = 8;
+  const startIndex = (pageNo - 1) * productsPerPage;
+  const endIndex = pageNo * productsPerPage;
+  const pageResult = products.slice(startIndex, endIndex);
+  res.json({
+    pageResult,
+    lastPage: Math.ceil(products.length / productsPerPage),
+  });
+});
+
+server.get("/product-list/", (req, res) => {
+  res.status(400).send("BAD REQUEST: NEED PAGE NUMBER PARAMETER");
+});
 
 server.use(cors(corsOptions));
 server.use(middlewares);
